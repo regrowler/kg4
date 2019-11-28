7513b2751e65ae7e037e93b621118e1fd39c9f0f
John Doe <johndoe@john.doe>
2019-11-28 11:02:03 +0400

    kg4

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace KG4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.open = new System.Windows.Forms.Button();
+            this.grey = new System.Windows.Forms.Button();
+            this.saw = new System.Windows.Forms.Button();
+            this.mask = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1073, 751);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(12, 12);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(117, 37);
+            this.open.TabIndex = 2;
+            this.open.Text = "OPEN FILE";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // grey
+            // 
+            this.grey.Location = new System.Drawing.Point(12, 76);
+            this.grey.Name = "grey";
+            this.grey.Size = new System.Drawing.Size(117, 37);
+            this.grey.TabIndex = 3;
+            this.grey.Text = "TURN TO GREY";
+            this.grey.UseVisualStyleBackColor = true;
+            this.grey.Click += new System.EventHandler(this.grey_Click);
+            // 
+            // saw
+            // 
+            this.saw.Location = new System.Drawing.Point(12, 136);
+            this.saw.Name = "saw";
+            this.saw.Size = new System.Drawing.Size(117, 37);
+            this.saw.TabIndex = 4;
+            this.saw.Text = "SAW FILTER";
+            this.saw.UseVisualStyleBackColor = true;
+            this.saw.Click += new System.EventHandler(this.saw_Click);
+            // 
+            // mask
+            // 
+            this.mask.Location = new System.Drawing.Point(12, 240);
+            this.mask.Name = "mask";
+            this.mask.Size = new System.Drawing.Size(117, 37);
+            this.mask.TabIndex = 5;
+            this.mask.Text = "MATRIX MASK";
+            this.mask.UseVisualStyleBackColor = true;
+            this.mask.Click += new System.EventHandler(this.mask_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 765);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.mask);
+            this.Controls.Add(this.saw);
+            this.Controls.Add(this.grey);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button grey;
+        private System.Windows.Forms.Button saw;
+        private System.Windows.Forms.Button mask;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KG4
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap bmp { get; set; }
+
+        public static Bitmap bmpAfterConvert { get; set; }
+
+        public static Bitmap bmpAfterConvertContrast { get; set; }
+
+        public static Bitmap bmpAfterConvertFilter { get; set; }
+
+        private int sawpicks = 4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                bmp = null;
+            }
+
+            openFileDialog1.Filter =
+                "JPEG (*.jpg; *.jpeg)|*.jpg; *.jpeg|Точечные рисунки (*.bmp)|*.bmp|GIF (*.gif)|*.gif|PNG (*png)|*.png";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bmp = new Bitmap(openFileDialog1.OpenFile());
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void grey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bmp != null)
+                {
+                    bmpAfterConvert = (Bitmap) bmp.Clone();
+                    Color color;
+                    byte color_after_convert;
+                    for (int i = 0; i < bmp.Width; i++)
+                    {
+                        for (int j = 0; j < bmp.Height; j++)
+                        {
+                            color = bmp.GetPixel(i, j);
+                            color.GetBrightness();
+                            color_after_convert = (byte) (0.299 * color.R + 0.587 * color.G + 0.114 * color.B);
+                            bmpAfterConvert.SetPixel(i, j,
+                                Color.FromArgb(color_after_convert, color_after_convert, color_after_convert));
+                        }
+                    }
+
+                    pictureBox1.Image = bmpAfterConvert;
+                }
+                else
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(
+                    "Ошибка обработки!\nВозможно, обрабатываемый файл отсутствует или не найден.\nПовторите попытку позже.",
+                    "Сообщение");
+            }
+        }
+
+        private void saw_Click(object sender, EventArgs e)
+        {
+            if (bmpAfterConvert == null)
+            {
+                grey_Click(null, null);
+            }
+            bmpAfterConvertContrast = (Bitmap) bmpAfterConvert.Clone();
+            for (int i = 0; i < bmpAfterConvertContrast.Width; i++)
+            {
+                for (int j = 0; j < bmpAfterConvertContrast.Height; j++)
+                {
+                    Color clr = bmp.GetPixel(i, j);
+                    int g = clr.R;
+                    int s = (256 / sawpicks);
+                    g = (g % s) * sawpicks;
+                    //if (g >= 0 && g <= 63)
+                    //{
+                    //    g = g * 4;
+                    //}
+                    //else if (g >= 64 && g <= 127)
+                    //{
+                    //    g = (g - 64) * 4;
+                    //}
+                    //else if (g >= 128 && g <= 191)
+                    //{
+                    //    g = (g - 128) * 4;
+                    //}
+                    //else
+                    //{
+                    //    g = (g - 192) * 4;
+                    //}
+
+                    bmpAfterConvertContrast.SetPixel(i, j, Color.FromArgb(clr.A, g, g, g));
+                    pictureBox1.Image = bmpAfterConvertContrast;
+                }
+            }
+        }
+
+        private void mask_Click(object sender, EventArgs e)
+        {
+            if (bmp != null)
+            {
+                bmpAfterConvertFilter = (Bitmap) bmp.Clone();
+                double[,] matrix = new double[3, 3];
+                matrix[0, 0] = 0;
+                matrix[1, 0] = -1;
+                matrix[2, 0] = 0;
+                matrix[0, 1] = -1;
+                matrix[1, 1] = 8;
+                matrix[2, 1] = -1;
+                matrix[0, 2] = 0;
+                matrix[1, 2] = -1;
+                matrix[2, 2] = 0;
+                int width = bmp.Width;
+                int height = bmp.Height;
+
+                int matrixWidth = matrix.GetLength(0);
+                int matrixHeight = matrix.GetLength(1);
+
+                //Производим вычисления
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        double rSum = 0, gSum = 0, bSum = 0,matrixsum=0;
+
+                        for (int i = 0; i < matrixWidth; i++)
+                        {
+                            for (int j = 0; j < matrixHeight; j++)
+                            {
+                                int pixelPosX = x + (i - 1);
+                                int pixelPosY = y + (j - 1);
+                                if ((pixelPosX < 0) ||
+                                  (pixelPosX >= width) ||
+                                  (pixelPosY < 0) ||
+                                  (pixelPosY >= height))
+                                    continue;
+
+                                byte r = bmp.GetPixel(pixelPosX, pixelPosY).R;
+                                byte g = bmp.GetPixel(pixelPosX, pixelPosY).G;
+                                byte b = bmp.GetPixel(pixelPosX, pixelPosY).B;
+
+                                double matrixVal = matrix[i, j];
+                                matrixsum += matrixVal;
+                                rSum += r * matrixVal;
+                                gSum += g * matrixVal;
+                                bSum += b * matrixVal;
+
+                                
+                            }
+                        }
+
+                        rSum /= matrixsum;
+                        if (rSum < 0)
+                        {
+                            rSum = 0;
+                        }
+                        if (rSum > 255)
+                        {
+                            rSum = 255;
+                        }
+
+                        gSum /= matrixsum;
+                        if (gSum < 0)
+                        {
+                            gSum = 0;
+                        }
+                        if (gSum > 255)
+                        {
+                            gSum = 255;
+                        }
+
+                        bSum /= matrixsum;
+                        if (bSum < 0)
+                        {
+                            bSum = 0;
+                        }
+                        if (bSum > 255)
+                        {
+                            bSum = 255;
+                        }
+
+                        //Записываем значения в результирующее изображение
+
+                        bmpAfterConvertFilter.SetPixel(x, y, Color.FromArgb((byte)rSum, (byte)gSum, (byte)bSum));
+                        
+                    }
+                }
+                pictureBox1.Image = bmpAfterConvertFilter;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Int32.TryParse(textBox1.Text,out sawpicks);
+        }
+    }
+}
